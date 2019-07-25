0db3db0d7191fc264dc44d546d5346c92b36a030
sarthak199 <53030934+sarthak199@users.noreply.github.com>
2019-07-25 10:59:06 +0530

    Update insertion.cpp

--- insertion.cpp
+++ insertion.cpp
@@ -3,19 +3,26 @@ using namespace std;
 
 int main()
 {
-  int a[5]={1,2,3,4,5};
-   cout<<"array before inserting "<<endl;
-    for(int i;i<5;i++)
-{
+  int a[50],e,n,i;
+    cout<<"enter the no of  element you want to enter "<<endl;
+  cin>>n;
+  cout<<"enter the element in array"<<endl;
+  for(i=0;i<=n;i++)
+  {
+    cin>>a[i];
+  }
+cout<<"array before inserting "<<endl;
+  for(int i;i<n;i++)
+  {
   cout<<a[i];
-}
+  }
   cout <<"inserting the array of index 2"<<endl;
   cin>>a[2];
   cout<<"array after inserting "<<endl;
-  for(int i;i<5;i++)
-{
+  for(int i;i<n;i++)
+  {
   cout<<a[i];
-}
+  }
   
 return 0;
 }
